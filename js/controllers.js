ebb03295b456ef5a5c930e6f7c72b8e91fcf0573
Sandro Scalco <info@sandrodesign.ch>
2015-06-01 21:32:04 +0200

    enhancements

--- js/controllers.js
+++ js/controllers.js
@@ -57,6 +57,29 @@ angular.module('starter.controllers', [])
       $state.go('tab.account');
     }
 
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+    var myBarChart = new Chart(ctx).Bar(data);
     // Code mal noch beahlten!!!!!
     /*
     $scope.data = {};
@@ -406,16 +429,16 @@ angular.module('starter.controllers', [])
     $scope.saveUser = function() {
 
       User.setUser($scope.user);
-/*
-      if ($scope.user.username !== null) {
-        window.localStorage.setItem("username", $scope.user.username); //sollte auch in store..
-      }
-      if ($scope.user.password !== null) {
-        window.localStorage.setItem("password", $scope.user.password); //sollte auch in store..
-      }
-      if ($scope.user.budget !== null) {
-        window.localStorage.setItem("budget", $scope.user.budget); //sollte auch in store..
-      }*/
+      /*
+            if ($scope.user.username !== null) {
+              window.localStorage.setItem("username", $scope.user.username); //sollte auch in store..
+            }
+            if ($scope.user.password !== null) {
+              window.localStorage.setItem("password", $scope.user.password); //sollte auch in store..
+            }
+            if ($scope.user.budget !== null) {
+              window.localStorage.setItem("budget", $scope.user.budget); //sollte auch in store..
+            }*/
       var authData = fb.getAuth();
       if (authData && isNewUser) {
         // save the user's profile into Firebase so we can list users,
