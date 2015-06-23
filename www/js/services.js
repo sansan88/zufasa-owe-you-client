4ffb44a37165137c4593dee54c65ae7797cf5cd9
Sandro Scalco <info@sandrodesign.ch>
2015-06-23 18:25:45 +0200

    number problem

--- www/js/services.js
+++ www/js/services.js
@@ -64,7 +64,7 @@ angular.module('starter.services', [])
         var budget = 0;
         if (window.localStorage.getItem("budget") !== undefined) {
           //budget = window.localStorage.getItem("budget");
-          budget = new Number(window.localStorage.getItem("budget"););
+          budget = new Number(window.localStorage.getItem("budget"));
         }
         return budget;
       },
